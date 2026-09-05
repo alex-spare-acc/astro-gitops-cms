b9302c31fcbea0533ffe0351a1cd1cd7d576ff04
Alexander Lee <olsvcs@yahoo.com>
2026-09-04 21:53:02 -0400

    fix build

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro:content/loaders';
+import { glob } from 'astro/loaders';
 
 const pagesCollection = defineCollection({
   loader: glob({ 
