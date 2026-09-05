92c902ab5abee75534e51723491988e837cf81ba
Alexander Lee <olsvcs@yahoo.com>
2026-09-04 21:37:51 -0400

    add astro router

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+
+const pagesCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    heroText: z.string()
+  })
+});
+
+export const collections = {
+  'pages': pagesCollection
+};
